758f2d57cd6bf106f442cb35c5d2f1d4351dd395
Joshua Otwori <jobootwori@gmail.com>
2022-12-22 21:08:45 +0300

    Classroom class

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,14 @@
+/**
+ * Represents a class room.
+ *
+ */
+export default class classRoom {
+  /**
+     * Creates a new @see {@link ClassRoom}.
+     * @param {Number} maxStudentsSize - The maximum number of
+     * students in the class.
+     */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
